40ee10470193efb56f4c9741b39df7684d9bb84f
Manuel <manuelinfosec@gmail.com>
2024-05-21 01:13:44 +0100

    Refactor main.ts: Add detailed comments and update server listen options

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,29 @@ import * as express from 'express';
 import config from './config';
 import loaders from './loaders';
 
+// Main function to initialize and start the Express application
 async function main() {
+  // Create an Express application instance
   const app = express.default();
+
+  // Load necessary configurations, middleware, routes, etc.
   await loaders(app);
 
-  app.listen(config.port, () => {
+  // Start the server with the specified options.
+  // - port: Port number from the configuration (read from environment variable)
+  // - hostname: Hostname on which the server will listen ('0.0.0.0' means listen on all available network interfaces).
+  // - backlog: Maximum length of the queue of pending connections.
+  app.listen({ port: config.port, hostname: '0.0.0.0', backlog: 511 }, () => {
     console.log(`Server is listening on port ${config.port}`);
   });
 
-  // graceful shutdown
+  // Setup graceful shutdown on receiving the SIGTERM signal
   process.on('SIGTERM', () => {
     console.log('SIGTERM signal received.');
     console.log('Express app closed.');
     process.exit(0);
   });
-
-  // app.listen(config.port, '0.0.0.0', (err: any) => {
-  //   if (err) {
-  //     console.log(err);
-  //     process.exit(1);
-  //     return;
-  //   }
-  //   console.log(`Server listening on port: ${config.port}`);
-  // });
 }
 
+// Entrypoint
 main();
